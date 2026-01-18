48f17fd74fba837e348e6f23c38615fedc026f0a
AllanTran90 <allan_sweden90@hotmail.com>
2026-01-18 20:12:48 +0100

    added some more code

--- space-invader/game.js
+++ space-invader/game.js
@@ -14,5 +14,37 @@ const H = canvas.height;
 // input
 const key = new Set();
 window.addEventListener("keydown", (e) => {
-    key.add(e.key.Lo)
-}
+    key.add(e.key.toLowerCase());
+
+    if (e.key.toLowerCase() === "r" && gameOver){
+        resetGame();
+    }
+});
+window.addEventListener("keydown", (e) => key.delete(e.key.toLowerCase()));
+
+// game start
+let gameOver =false;
+let score = 0;
+let level = 1;
+
+// highscore(localstorage)
+let highScore = localStorage.getItem("spaceInvaderHighScore");
+highScore = highScore ? Number(highScore) : 0;
+highScoreEl.textContent = highScore;
+
+// player
+const player = {
+    x: W / 2 - 18,
+    y: H - 52,
+    w: 36,
+    h: 16
+};
+
+const playerStats = {
+    fireRate: 300,
+    bulletSpeed: 7,
+    moveSpeed: 4,
+    bulletCount: 1
+};
+
+// object
